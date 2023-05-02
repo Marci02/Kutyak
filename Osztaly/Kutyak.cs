2d8d080ed6bec56fabcd85f78916fa9a44414ce4
Marci02 <nagymarci.0218@gmail.com>
2023-05-02 12:59:12 +0200

    Add project files.

---
+++ Osztaly/Kutyak.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Osztaly
+{
+    class Kutyak
+    {
+        public static BinaryWriter writer;
+        public static BinaryReader reader;
+
+        string _fajta;
+        string _neve;
+        int _kora;
+
+        public Kutyak(string fajta, string neve, int kora)
+        {
+            _fajta = fajta;
+            _neve = neve;
+            _kora = kora;
+        }
+
+        public Kutyak()
+        {
+            LoadFromFile();
+        }
+
+        public void SavetoFile()
+        {
+            Kutyak.writer.Write(_fajta);
+            Kutyak.writer.Write(_neve);
+            Kutyak.writer.Write(_kora);
+        }
+
+        public void LoadFromFile()
+        {
+
+            _fajta = Kutyak.reader.ReadString();
+            _neve = Kutyak.reader.ReadString();
+            _kora = Kutyak.reader.ReadInt32();
+        }
+
+        public void SetFajta(string value)
+        {
+            _fajta = value;
+        }
+        
+        public string GetFajta()
+        {
+            return _fajta;
+        }
+        
+        public void SetName(string value2)
+        {
+            _neve = value2;
+        }
+
+
+        public string GetName()
+        {
+            return _neve;
+        }
+
+
+        public void SetAge(int value3)
+        {
+           _kora = value3;
+        }
+
+        public int GetAge()
+        {
+            return _kora;
+        }
+    }
+}
---
+++ Osztaly/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Osztaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<Kutyak> dogs = new List<Kutyak>();
+
+            FileStream fs = new FileStream("dogs.db", FileMode.OpenOrCreate);
+
+            Kutyak.reader = new BinaryReader(fs);
+
+
+            //Kutyak dog1 = new Kutyak("Vizsla", "Dió", 3);
+            //Kutyak dog2 = new Kutyak("Puli", "Maszat", 5);
+            //Kutyak dog3 = new Kutyak("Kuvasz", "Bogi", 8);
+
+
+            while (fs.Length > fs.Position)
+            {
+                dogs.Add(new Kutyak());
+            }
+
+
+            //Kutyak dog1 = new Kutyak();
+            //Kutyak dog2 = new Kutyak();
+            //Kutyak dog3 = new Kutyak();
+
+            fs.Close();
+
+            foreach (Kutyak dog in dogs)
+            {
+                Console.WriteLine($"{dog.GetFajta()}, {dog.GetName()}, {dog.GetAge()}");
+
+            }
+
+          
+
+            FileStream fs1 = new FileStream("dogs.db", FileMode.OpenOrCreate);
+
+            Kutyak.writer = new BinaryWriter(fs1);
+
+            foreach (Kutyak dog in dogs)
+            {
+                dog.SavetoFile();
+            }
+
+
+            //dog1.SavetoFile();
+            //dog2.SavetoFile();
+            //dog3.SavetoFile();
+
+            fs1.Close();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
